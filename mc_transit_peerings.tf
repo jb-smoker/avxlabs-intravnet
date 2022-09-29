69ecd8306f0b96943ed974342740bce5e24a7342
John Smoker <jsmoker@aviatrix.com>
2022-09-29 12:54:25 -0700

    use transit peering module

--- mc_transit_peerings.tf
+++ mc_transit_peerings.tf
@@ -1,9 +1,14 @@
-resource "aviatrix_transit_gateway_peering" "aws_to_azure" {
-  transit_gateway_name1 = module.aws_transit.mc_firenet_details.name
-  transit_gateway_name2 = module.azure_transit.mc_firenet_details.name
-}
+module "transit-peering" {
+  source  = "terraform-aviatrix-modules/mc-transit-peering/aviatrix"
+  version = "1.0.7"
 
-resource "aviatrix_transit_gateway_peering" "aws_to_gcp" {
-  transit_gateway_name1 = module.aws_transit.mc_firenet_details.name
-  transit_gateway_name2 = module.gcp_transit.mc_firenet_details.name
+  transit_gateways = [
+    module.aws_transit.transit_gateway.gw_name,
+    module.azure_transit.transit_gateway.gw_name,
+    module.gcp_transit.transit_gateway.gw_name
+  ]
+
+  excluded_cidrs = [
+    "0.0.0.0/0",
+  ]
 }
